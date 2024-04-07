18e58ef3e5303178ca095a25c1e7cf0b75ab2d3b
pc <cybermentors.india@gmail.com>
2024-04-07 17:03:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
@@ -37,10 +38,21 @@ function App() {
       <p className='p-3 m-0 text-center'>Developed and maintained by Cybermentors</p></footer>
 =======
 import logo from './logo.svg';
+=======
+>>>>>>> b7d9f6b (first commit)
 import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Accomplishment from './pages/Accomplishment';
+import About from './pages/About';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -57,6 +69,32 @@ function App() {
         </a>
       </header>
 >>>>>>> c62689a (Initialize project using Create React App)
+=======
+    <div>
+      <Navbar expand="lg" className="bg-success	shadow">
+        <Container>
+          <Navbar.Brand href="/"  className='navbar-brand text-white fw-semibold'>Jagat Cricket Academy</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto justify-content-end w-100">
+            <Nav.Link href='/' className='active text-uppercase text-white'>Home</Nav.Link>
+            <Nav.Link href='/accomplishment' className='text-uppercase text-white'>Accomplishments</Nav.Link>
+            <Nav.Link href='/about' className='text-uppercase text-white'>About</Nav.Link>
+            <Nav.Link href='/contact' className='text-uppercase text-white'>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/accomplishment' element={<Accomplishment />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+    <footer className='bg-body-tertiary'>
+      <p className='p-3 m-0 text-center'>Developed and maintained by Cybermentors</p></footer>
+>>>>>>> b7d9f6b (first commit)
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter}  from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,15 +16,22 @@ root.render(
 
 =======
 
+=======
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter}  from 'react-router-dom';
+>>>>>>> b7d9f6b (first commit)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
+<<<<<<< HEAD
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 >>>>>>> c62689a (Initialize project using Create React App)
+=======
+>>>>>>> b7d9f6b (first commit)
